de3b3b3e2d299b0f4f0ce12e3fb886d300362e65
Michael Fatemi <myfatemi04@gmail.com>
2022-04-28 13:08:00 -0400

    random forests!!

--- pid-sim.py
+++ pid-sim.py
@@ -54,29 +54,107 @@ def clamp(x):
     return max(min(x, 640), 0)
 
 
-def plot_random_sampling():
+def metamodel():
+    import pickle
+    import os
+
     np.random.seed(42)
 
-    result_k2 = []
-    result_k3 = []
-    result_values = []
-    for i in range(1000):
-        k1 = 1
-        k2 = np.random.rand() * 10
-        k3 = np.random.rand() * 10
-        result_k2.append(k2)
-        result_k3.append(k3)
-        result_values.append(get_pid_runtime(k1, k2, k3))
+    if os.path.exists("./data.pkl"):
+        with open("./data.pkl", "rb") as f:
+            X, y = pickle.load(f)
+    else:
+        X, y = run_random_sampling(plot=False)
 
-    plt.title("Effect of k2 and k3 on velocity")
-    plt.scatter(result_k2, result_k3, c=result_values)
-    plt.xlabel("K2")
-    plt.ylabel("K3")
-    plt.colorbar()
+        with open("./data.pkl", "wb") as f:
+            pickle.dump((X, y), f)
+
+    print("Loaded training samples")
+
+    from sklearn.model_selection import train_test_split
+
+    y = y[:, np.newaxis]
+    X_train, X_test, y_train, y_test = train_test_split(X, y)
+
+    method = 'random_forest'
+
+    if method == 'dense_network':
+        import tensorflow as tf
+        import tensorflow.keras.models as models
+        import tensorflow.keras.layers as layers
+
+        regressor = models.Sequential([
+            layers.Normalization(axis=-1),
+            layers.Dense(16, input_shape=(2,), activation='sigmoid'),
+            layers.Dense(8, activation='sigmoid'),
+            layers.Dense(1, activation='relu'),
+        ])
+        regressor.compile(optimizer='adam', loss='mse', metrics=['mae'])
+
+        regressor.fit(X_train, y_train, epochs=100)
+        y_pred = regressor.predict(X_test)[:, 0]
+    elif method == 'random_forest':
+        from sklearn.ensemble import RandomForestRegressor
+
+        regressor = RandomForestRegressor()
+        regressor.fit(X_train, y_train)
+
+        import time
+
+        start_time = time.time()
+        y_pred = regressor.predict(X_test)
+        end_time = time.time()
+
+        print(f"Random forest regression time: {end_time - start_time:.5f}")
+
+        result = regressor.score(X_test, y_test)
+
+        print("Scoring")
+        print(result)
+
+    plt.subplots_adjust(hspace=0.5)
+
+    plt.subplot(2, 1, 1)
+    plt.title("Real")
+    plt.scatter(X_train[:, 0], X_train[:, 1], c=(y_train[:, 0]))
+
+    plt.subplot(2, 1, 2)
+    plt.title("Predicted")
+    plt.scatter(X_test[:, 0], X_test[:, 1], c=y_pred)
+
     plt.show()
 
 
-def plot_image():
+def run_random_sampling(plot=True):
+    import time
+
+    n = 2000
+    result_points = np.zeros((n, 2))
+    result_values = []
+    start_time = time.time()
+    for i in range(n):
+        k1 = 1
+        k2 = np.random.rand() * 10
+        k3 = np.random.rand() * 10
+        result_points[i] = [k2, k3]
+        # np.append(result_points, [k2, k3], axis=0)
+        result_values.append(get_pid_runtime(k1, k2, k3))
+    end_time = time.time()
+
+    print(f"Elapsed time: {end_time - start_time:.2f}")
+
+    if plot:
+        plt.title("Effect of k2 and k3 on velocity")
+        plt.scatter(result_points[:, 0], result_points[:, 1], c=result_values)
+        plt.xlabel("K2")
+        plt.ylabel("K3")
+        plt.colorbar()
+        plt.show()
+
+    return result_points, np.array(result_values)
+
+
+def run_imaging():
     n_k2 = 250
     n_k3 = 250
     results = np.zeros((n_k2, n_k3))
@@ -97,7 +175,9 @@ def plot_image():
     plt.show()
 
 
-plot_random_sampling()
+# run_random_sampling()
+
+metamodel()
 
 # running = True
 # while running:
