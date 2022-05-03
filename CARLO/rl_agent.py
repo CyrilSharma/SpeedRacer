fcd4b32cbffa5f0fe615b3b1eef8aae9a38b492a
Michael Fatemi <myfatemi04@gmail.com>
2022-05-03 14:57:11 -0400

    stuff + cv2 vis

--- CARLO/rl_agent.py
+++ CARLO/rl_agent.py
@@ -8,6 +8,8 @@ from world import World
 from agents import Car
 from lidar import read_lidar
 
+import cv2
+
 
 class RLCar(nn.Module):
     def __init__(self, input_size: int):
@@ -25,8 +27,8 @@ class RLCar(nn.Module):
         self.fc2 = nn.Linear(128, 16)
         self.fc3 = nn.Linear(16, 16)
 
-        # Forward, backward, left, right
-        self.output = nn.Linear(16, 4)
+        # Forward/backward, left/right
+        self.output = nn.Linear(16, 2)
 
     def forward(self, x: torch.Tensor):
         # x = F.relu(self.conv1(x))
@@ -51,12 +53,12 @@ class Simulator:
         self.dt = world.dt
 
     def step(self, forward_or_backward, left_or_right):
-        if forward_or_backward == 0:
+        if forward_or_backward >= 0.5:
             self.throttle += 1.5
         else:
             self.throttle -= 1.5
 
-        if left_or_right == 0:
+        if left_or_right >= 0.5:
             self.steering += 0.5
         else:
             self.steering -= 0.5
@@ -76,8 +78,12 @@ class Simulator:
 
 
 def train(agent: RLCar, simulators: List[Simulator]):
+    canvas = np.zeros((400, 400, 3), dtype=np.uint8)
+
+    eps_greedy = 0.1
+
     gamma = 0.7
-    optimizer = torch.optim.Adam(agent.parameters(), lr=0.001)
+    optimizer = torch.optim.Adam(agent.parameters(), lr=0.03)
     for it, simulator in enumerate(simulators):
         # Run simulation.
         # Array of (state, action, reward, next_state)
@@ -92,50 +98,84 @@ def train(agent: RLCar, simulators: List[Simulator]):
 
             action = agent(processed_state)[0]
 
-            forward_or_backward = torch.argmax(action[:2])
-            left_or_right = torch.argmax(action[2:])
+            canvas[:] = 0
+            center = simulator.car.center
+            x, y = center.x, center.y
+            cv2.circle(canvas, (int(x), int(y)), 5, (255, 255, 255), -1)
+
+            cv2.imshow("Car", canvas)
+            cv2.waitKey(1)
+
+            was_random = torch.rand(1) < eps_greedy * (np.exp(-runtime / 1000))
 
             # THIS NEEDS TO BE FIXED
-            simulator.step(forward_or_backward, left_or_right)
+            if was_random:
+                simulator.step(torch.rand(1), torch.rand(1))
+            else:
+                simulator.step(action[0], action[1])
 
             # Q(s, a) = r + gamma * max_a' Q(s', a')
             # Q(s) = [r(a) + gamma * max_a' Q(s', a') for a in action space]
 
-            collided = False
+            end = False
 
             reward = -1 + simulator.car.velocity.norm()
+
             if simulator.collided():
                 print(it, "Collided after", runtime, "steps")
-                collided = True
-                reward += -10
+                end = True
+                reward += -100
+            if simulator.car.velocity.norm() == 0:
+                reward += -50
+                end = True
 
             next_state = simulator.get_state()
 
-            history.append((state, action, reward, next_state))
+            if not was_random:
+                history.append((state, action, reward, next_state))
 
-            if collided:
+            if end:
                 break
         else:
             print(it, "Finished after", runtime,
                   "steps and travelled", total_distance, "m")
 
         print(" - Final car velocity:", simulator.car.velocity.norm())
+        print(" - Example output:", action)
 
         # Discount rewards and run backprop.
         optimizer.zero_grad()
 
         total_loss = 0
         running_reward = 0
+        discounted_rewards = []
         for i in range(len(history) - 1, -1, -1):
             state, action, reward, next_state = history[i]
             running_reward = running_reward * gamma + reward
+            discounted_rewards.append(running_reward)
+
+        discounted_rewards = discounted_rewards[::-1]
+        discounted_rewards = (
+            discounted_rewards - np.mean(discounted_rewards)) / np.std(discounted_rewards)
+
+        for i in range(len(history)):
+            state, action, reward, next_state = history[i]
+
+            discounted_reward = discounted_rewards[i]
 
             eps = 1e-5
 
-            highest_p_fb = -torch.log(torch.max(action[:2]) + eps)
-            highest_p_lr = -torch.log(torch.max(action[2:]) + eps)
+            if action[0] > 0.5:
+                highest_p_fb = -torch.log(action[0] + eps)
+            else:
+                highest_p_fb = -torch.log((1 - action[0]) + eps)
 
-            loss = highest_p_fb * reward + highest_p_lr * reward
+            if action[1] > 0.5:
+                highest_p_lr = -torch.log(action[1] + eps)
+            else:
+                highest_p_lr = -torch.log((1 - action[1]) + eps)
+
+            loss = highest_p_fb * discounted_reward + highest_p_lr * discounted_reward
             loss.backward()
 
             total_loss += loss.item()
@@ -158,7 +198,7 @@ if __name__ == "__main__":
         # A Car object is a dynamic object -- it can move. We construct it using its center location and heading angle.
         car = Car(Point(91.75, 60), np.pi/2)
         car.max_speed = 30.0  # let's say the maximum is 30 m/s (108 km/h)
-        car.velocity = Point(0, 3.0)
+        car.velocity = Point(0.0, 1.0)
 
         return Simulator(CircularWorld, car, 64)
 
